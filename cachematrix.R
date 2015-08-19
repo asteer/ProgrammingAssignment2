39d29796abe9474415c0e15a9fc8ba5aab7cb356
asteer <steer.adrienne@gmail.com>
2015-08-19 10:10:12 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions caches the inverse of a matrix and allows a user
+## to either retreive the cached value or write the new value if applicable.
+## Fuctions are based on example code from Coursera course rprog-031
+## https://class.coursera.org/rprog-031/human_grading/view/courses/975105/assessments/3/submissions
 
-## Write a short comment describing this function
+
+## first function (makeCacheMatrix) creates a vector in which 
+## to store the inverse value
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(mean) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## second function (cacheSolve) checks for existing value, 
+## computes the inverse if not found and tells user which it is doing
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
